ddc3d0af372eead88e513fb4c80c20088f37a9f2
Javier Almeida <108470987+javieralmeida30@users.noreply.github.com>
2023-06-16 19:21:55 -0300

    Add files via upload

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,1 @@
+ami_id = "ami-0e820afa569e84cc1"
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "aws_access_key" {
+    description = "Access Key"
+}
+
+variable "aws_secret_key" {
+  description = "secret Key"
+}
